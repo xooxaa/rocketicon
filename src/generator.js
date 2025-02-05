090942d3f3ad1ebd628f2be6b2df3ae5f7cfcae9
xooxaa <37028661+xooxaa@users.noreply.github.com>
2025-02-05 08:14:34 +0100

    update colors; add example mages

--- src/generator.js
+++ src/generator.js
@@ -5,22 +5,22 @@ export function generateRocketSVG(input) {
   const hash = hashInput(input);
 
   const [noseIndex, bodyIndex, finIndex, exhaustIndex] = mapHashToIndices(hash, [
-    rocketParts.noses.length,
     rocketParts.bodies.length,
+    rocketParts.noses.length,
     rocketParts.fins.length,
     rocketParts.exhausts.length,
   ]);
 
-  const nose = rocketParts.noses[noseIndex];
   const body = rocketParts.bodies[bodyIndex];
-  const fin = rocketParts.fins[finIndex];
+  const nose = rocketParts.noses[noseIndex];
+  const fins = rocketParts.fins[finIndex];
   const exhaust = rocketParts.exhausts[exhaustIndex];
 
   return `
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 200" width="100" height="200">
-      <path d="${nose}" fill="#FF5733" />
       <path d="${body}" fill="#33C3FF" />
-      <path d="${fin}" fill="#FFC300" />
+      <path d="${nose}" fill="#FF5733" />
+      <path d="${fins}" fill="#FFC300" />
       <path d="${exhaust}" fill="#C70039" />
     </svg>
   `;
--- src/rocketParts.js
+++ src/rocketParts.js
@@ -1,11 +1,11 @@
 export const rocketParts = {
-  noses: [
-    "M50 0 L90 50 L10 50 Z", // Example triangular nose
-    "M50 0 C60 20, 40 20, 50 50 Z", // Curved nose
-  ],
   bodies: [
     "M20 50 L80 50 L80 150 L20 150 Z", // Rectangular body
     "M30 50 C70 100, 70 100, 30 150 Z", // Tapered body
+  ],
+  noses: [
+    "M50 0 L90 50 L10 50 Z", // Example triangular nose
+    "M50 0 C60 20, 40 20, 50 50 Z", // Curved nose
   ],
   fins: [
     "M20 150 L50 130 L50 150 Z", // Simple fin
