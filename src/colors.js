551f5b7c0d89cff51412b46c830bfcb7614d01c3
xooxaa <j-suhrheinrich@gmx.de>
2025-02-06 22:42:43 +0100

    add rocketColors array to allow for random colored parts; update rocket parts to hae a a basic working rocket;

---
+++ src/colors.js
@@ -0,0 +1,6 @@
+export const rocketColors = {
+  bodies: ["#e9ecef"],
+  noses: ["#ef476f"],
+  fins: ["#118ab2", "#06d6a0"],
+  exhausts: ["#ffd166"],
+};
--- src/generator.js
+++ src/generator.js
@@ -1,27 +1,36 @@
 import { rocketParts } from "./rocketParts.js";
+import { rocketColors } from "./colors.js";
 import { hashInput, mapHashToIndices } from "./hashUtils.js";
 
 export function generateRocketSVG(input) {
   const hash = hashInput(input);
 
-  const [noseIndex, bodyIndex, finIndex, exhaustIndex] = mapHashToIndices(hash, [
+  const [noseIndex, bodyIndex, finIndex, exhaustIndex, noseColorIndex, bodyColorIndex, finColorIndex, exhaustColorIndex] = mapHashToIndices(hash, [
     rocketParts.bodies.length,
     rocketParts.noses.length,
     rocketParts.fins.length,
     rocketParts.exhausts.length,
+    rocketColors.noses.length,
+    rocketColors.bodies.length,
+    rocketColors.fins.length,
+    rocketColors.exhausts.length,
   ]);
 
   const body = rocketParts.bodies[bodyIndex];
   const nose = rocketParts.noses[noseIndex];
   const fins = rocketParts.fins[finIndex];
   const exhaust = rocketParts.exhausts[exhaustIndex];
+  const bodyColor = rocketColors.bodies[bodyColorIndex];
+  const noseColor = rocketColors.noses[noseColorIndex];
+  const finsColor = rocketColors.fins[finColorIndex];
+  const exhaustColor = rocketColors.exhausts[exhaustColorIndex];
 
   return `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 200" width="100" height="200">
-      <path d="${body}" fill="#33C3FF" />
-      <path d="${nose}" fill="#FF5733" />
-      <path d="${fins}" fill="#FFC300" />
-      <path d="${exhaust}" fill="#C70039" />
-    </svg>
+<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" width="100" height="100" stroke="#000000">
+  <path d="${fins}" fill="${finsColor}" />
+  <path d="${exhaust}" fill="${exhaustColor}" />
+  <path d="${body}" fill="${bodyColor}" />
+  <path d="${nose}" fill="${noseColor}" />
+</svg>
   `;
 }
